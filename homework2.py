b84b2303f527e058257a1f967b26af84628e0e6d
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2024-07-21 18:53:52 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_homeworks = 12
+count_hours = 1.5
+course_name = 'Python'
+time_for_homework = count_hours / count_homeworks
+print('Курс:', course_name, 'всего задач:', count_homeworks, 'затрачено часов:', count_hours, 'среднее время выполнения:', time_for_homework, 'часа')
